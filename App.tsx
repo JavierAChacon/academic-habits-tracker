8cfe4d188ea347f6597743f19bfd9150dac23ce0
JavierAChacon <alejandro.chr@outlook.es>
2024-08-06 16:26:58 -0400

    feat: add home screen

--- App.tsx
+++ App.tsx
@@ -3,10 +3,12 @@ import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import SingUpScreen from "./src/screens/SingUpScreen"
 import SingInScreen from "./src/screens/SingInScreen"
+import HomeScreen from "./src/screens/HomeScreen"
 
 export type RootStackParamList = {
   SignUpScreen: undefined
   SignInScreen: undefined
+  HomeScreen: undefined
 }
 
 const Stack = createNativeStackNavigator<RootStackParamList>()
@@ -21,6 +23,7 @@ const App = () => {
       >
         <Stack.Screen name="SignUpScreen" component={SingUpScreen} />
         <Stack.Screen name="SignInScreen" component={SingInScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
